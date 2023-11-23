9ada8f6a16cbf041d7c8ca339a357d5718981aed
Patrick Mukherjee <patrick@mukherjee.de>
2023-11-23 21:35:16 +0100

    improved the output

--- config/chording/find_same_combo.py
+++ config/chording/find_same_combo.py
@@ -2,25 +2,24 @@ import sys
 from itertools import combinations
 
 def find_same_parameters(lines):
-    parameter_lines = {}
+    keys_word = {}
     
     # Iterate through each line
     for line in lines:
         if not line.startswith('CHORD_LAYER('):
             continue
+        line = line[len("CHORD_LAYER("):-1]
         # Extract the parameters from the line
-        parameters = line.split(', ')[2]  # Extract 'P_' parameters, skipping every other element
-        # Sort the parameters to handle the order indifference
-        sorted_parameters = ' '.join(sorted(parameters.split()))        
-        # Add the line to the dictionary based on sorted parameters
-        if sorted_parameters not in parameter_lines:
-            parameter_lines[sorted_parameters] = []
-        parameter_lines[sorted_parameters].append(line)
-    
-    # Find lines with the same parameters
-    result_lines = [lines for lines in parameter_lines.values() if len(lines) > 1]
-    
-    return result_lines
+        split_line = line.split(', ')
+        word = split_line[0]  # Extract the chorded word, skipping every other element
+        keys = split_line[2]  # Extract 'P_' keys, skipping every other element
+        # Sort the keys to handle the order indifference
+        sorted_keys = ' '.join(sorted(keys.split()))        
+        # Add the line to the dictionary based on sorted keys
+        if sorted_keys not in keys_word:
+            keys_word[sorted_keys] = []
+        keys_word[sorted_keys].append(word)
+    return keys_word
 
 def read_lines_from_file(file_path):
     with open(file_path, 'r') as file:
@@ -40,14 +39,22 @@ if __name__ == "__main__":
         input_lines = read_lines_from_file(file_name)
 
         # Find lines with the same parameters
-        result = find_same_parameters(input_lines)
+        parsed_lines = find_same_parameters(input_lines)
 
-        # Print the result
-        for lines in result:
+        # Find lines with the same parameters
+
+        same_lines = dict(filter(lambda item: len(item[1]) > 2, parsed_lines.items()))
+
+        # Print the result if same_lines isn't empty
+        if same_lines:
             print("Same parameters found:")
-            for line in lines:
-                print(f"  {line}")
-
+            for keys, chords in same_lines.items():
+                print(f"  {keys}: {chords}")
+        else:
+            print("# Chords::")
+            for keys, chord in parsed_lines.items():
+                    print(f"  {chord}: {keys}")
+                
     except FileNotFoundError:
         print(f"File not found: {file_name}")
     except Exception as e:
